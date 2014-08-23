06c7ed390c6d8500e15f1f4e2fbad24266f7a42b
Not Cookied Workshop <eshoprworkshop@gmail.com>
2014-08-23 20:14:34 +0300

    data displayed by directory

--- client/js/graphs/graphsController.js
+++ client/js/graphs/graphsController.js
@@ -7,26 +7,15 @@ graphsController.controller('graphsController', function($scope){
     	lines: { show: true },
     	colors:[ '#0055CC', '#078600', '#6e0069', '#ff8900' ],
 	};
-  
+
   $(function () {
-      var data = [ [[2003, 10882],
-          [2002, 10383],
-          [2001, 10020],
-          [2000, 9762],
-          [1999, 9213],
-          [1998, 8720]] ];
-       
-      var plotarea = $("#plotarea");
-      plotarea.css("height", "250px");
-      plotarea.css("width", "500px");
-      $.plot( plotarea , data );
+
   });
 
 	$scope.graphHeight = 400;
 
 	$scope.graphData = [[0, 3], [4, 8], [8, 5], [9, 13]];
-	// alert('tfo');
-	// $(placeholder).plot([[[0, 3], [4, 8], [8, 5], [9, 13]]]);
+
 })
 
 var graphsDirective = angular.module('graphsDirective', []);
@@ -34,14 +23,28 @@ graphsDirective.directive( 'chart', function() {
     return {
       restrict: 'AE',
       replace: true,
-      template: '<div id="plot-one" ng-model="hello" background-color:{{color}}">Hello World',
+      template: '<div class="plotarea">something',
       link: function(scope, elem, attrs) {
-        elem.bind('click', function() {
-          elem.css('background-color', 'white');
-          scope.$apply(function() {
-            scope.color = "white";
-          });
-        });
+        console.log(scope);
+        console.log(elem);
+        var data = [ [[2003, 10882],
+          [2002, 10383],
+          [2001, 10020],
+          [2000, 9762],
+          [1999, 9213],
+          [1998, 8720]] ];
+       
+      // var plotarea = $(".plotarea");
+      
+      // plotarea.css("width", "500px");
+      $.plot(elem, data );
+
+        // elem.bind('click', function() {
+        //   elem.css('background-color', 'white');
+        //   scope.$apply(function() {
+        //     scope.color = "white";
+        //   });
+        // });
         // elem.bind('mouseover', function() {
         //   elem.css('cursor', 'pointer');
         // });
