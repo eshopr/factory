84c60d817c0ff834ebb416501108ee7da70c88ca
Not Cookied Workshop <eshoprworkshop@gmail.com>
2014-08-27 15:29:57 +0300

    makes call authorization refused

--- server/controllers/translate.js
+++ server/controllers/translate.js
@@ -1,15 +1,35 @@
+var curl = require('curlrequest');
 // var passport =  require('passport')
 //     , User = require('../models/User.js');
-
 module.exports = {
     translate: function(req, res, next) {
-    	console.log(req);
-    	// return req
-    	try {
-            res.send(req.body);
-        }
-        catch(err) {
-            return res.send(400, err.message);
-        }
-    },
-};
+    	var dataObject = {   
+		    "text":"In the era of Siri",
+		    "target_language":"pt",
+		    "callback_url": "http://news.unbabel.co/unbabel_endpoint/"
+		}
+		,	api_token = "binarygeometry"
+		,	ApiKey = "#"
+
+		// ,	headers "Authorization: ApiKey username:api_token" 
+		var options = {
+		    url: 'http://www.unbabel.co/tapi/v2/translation/',
+		    headers: {
+		    	"Authorization": ApiKey,
+		    	// "username:": api_token,
+		    	"Content-Type": "application/json"
+		    },
+			method: "POST",
+			data: dataObject,
+			verbose: true,
+			stderr: true
+		};
+
+		curl.request(options, function (err, data) {
+			console.log(err, data);
+		});
+	}
+}
+
+
+curl -H "Authorization: ##### username: binarygeometry" -H "Content-Type: application/json" -X POST http://www.unbabel.co/tapi/v2/translation/ 
